c7e2e8f5f248081a41d01d371f9e6ed94845a661
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-22 12:54:36 -0600

    fix: add presence validation for status in Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,8 @@ class Invoice < ApplicationRecord
   has_many :products, through: :invoice_items
   has_many :transactions, dependent: :destroy
 
-  validates :status, presence: true
   enum status: { pending: 0, shipped: 1, completed: 2, canceled: 3 }
+
+  validates :status, presence: true
 end
+
