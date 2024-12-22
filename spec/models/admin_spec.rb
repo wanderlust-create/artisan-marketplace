06c85ab2c4016798a9a9acb8de25ddaaa0aa712e
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-22 11:56:39 -0600

    test: add validations for email presence and format in Admin model

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { create(:admin) }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value('test@example.com').for(:email) }
+    it { should_not allow_value('not-an-email').for(:email) }
+  end
+
+  describe 'secure password' do
+    it { should have_secure_password }
+  end
 end
+
+
