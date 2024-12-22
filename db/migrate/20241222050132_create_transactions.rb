ca111220f77046118a306cc1c648c9b767a97e64
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-21 23:01:53 -0600

    create Transactions model

---
+++ db/migrate/20241222050132_create_transactions.rb
@@ -0,0 +1,14 @@
+class CreateTransactions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :transactions do |t|
+      t.references :invoice, null: false, foreign_key: true
+      t.string :credit_card_number
+      t.string :credit_card_expiration_date
+      t.integer :status
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
