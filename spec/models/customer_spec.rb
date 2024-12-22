18f8d3f0e917bab7039337732606186091cc4b14
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-22 11:12:43 -0600

    test: add associations and validations for Customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:invoices).dependent(:destroy) }
+    it { should have_many(:reviews).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value('test@example.com').for(:email) }
+    it { should_not allow_value('not-an-email').for(:email) }
+  end
 end
+
