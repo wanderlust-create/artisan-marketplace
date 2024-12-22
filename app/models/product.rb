573995cceac67226b3acdebff47a52d6c8d4048f
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-22 12:25:39 -0600

    feat: add associations and validations to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :artisan
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items
+  has_many :reviews, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, :price, :stock, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
+
 
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:artisan) }
+    it { should have_many(:invoice_items).dependent(:destroy) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:reviews).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of(:stock) }
+    it { should validate_numericality_of(:stock).only_integer.is_greater_than_or_equal_to(0) }
+  end
 end
+
