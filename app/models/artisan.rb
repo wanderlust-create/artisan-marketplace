01359843a814d561fcb3d94b7feffa0b28c0982b
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-22 00:24:08 -0600

    feat: add shoulda-matchers gem and implement validations for Artisan model

--- app/models/artisan.rb
+++ app/models/artisan.rb
@@ -1,8 +1,8 @@
 class Artisan < ApplicationRecord
-  has_many :products, dependent: :destroy
-
   has_secure_password
+  validates :email, presence: true, uniqueness: true
 
+  validates :store_name, presence: true
   validates :email, presence: true, uniqueness: true
 end
 
--- spec/models/artisan_spec.rb
+++ spec/models/artisan_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Artisan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:products).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:store_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should have_secure_password }
+  end
 end
+
