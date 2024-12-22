301ffad5dce8c1ca5da1ca142a76693f8ff0d25a
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-22 12:28:08 -0600

    refactor: update Product model and specs to use RSpec's expected syntax

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,5 +8,3 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
-
-
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'associations' do
-    it { should belong_to(:artisan) }
-    it { should have_many(:invoice_items).dependent(:destroy) }
-    it { should have_many(:invoices).through(:invoice_items) }
-    it { should have_many(:reviews).dependent(:destroy) }
+    it { is_expected.to belong_to(:artisan) }
+    it { is_expected.to have_many(:invoice_items).dependent(:destroy) }
+    it { is_expected.to have_many(:invoices).through(:invoice_items) }
+    it { is_expected.to have_many(:reviews).dependent(:destroy) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:price) }
-    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
-    it { should validate_presence_of(:stock) }
-    it { should validate_numericality_of(:stock).only_integer.is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_presence_of(:stock) }
+    it { is_expected.to validate_numericality_of(:stock).only_integer.is_greater_than_or_equal_to(0) }
   end
 end
-
