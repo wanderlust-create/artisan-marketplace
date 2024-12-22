08b46148189cbb1711a12af96f85cb58c8e3d559
Tamara Dowis <tamara.dowis@gmail.com>
2024-12-22 00:44:40 -0600

    feat: configure SimpleCov for test coverage with minimum coverage requirement

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/'
+  SimpleCov.minimum_coverage 90
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
